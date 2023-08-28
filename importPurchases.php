dac9329ecd45454d5ec717f6cec4861416e113af
TealDolphin <44395321+TealDolphin@users.noreply.github.com>
2023-08-27 18:32:50 -0700

    Create importPurchases.php

---
+++ importPurchases.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once model.php;
+
+
+$f = fopen('%USERPROFILE%\\Downloads\\TODO.example', 'r') or die("Unable to open file!");
+
+
+
+
+
+
+
+
+
+// $theDBA->totalStudents($level);
+
+/*
+$f = fopen("dev/tempBan.log", "a") or die("Unable to open file!");
+fwrite($f, $_SERVER['REMOTE_ADDR'] . '<::>' . strval(time()) . "\n");
+fclose($f);
+*/
+
+?>
