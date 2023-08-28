96548a5bcc2d900c71cd918f632814f0c6171fd0
TealDolphin <44395321+TealDolphin@users.noreply.github.com>
2023-08-27 18:35:06 -0700

    Update control.php
    $theDBA-> additions
    history dummy function
    dollar => cent conversions
    assignParent command section

--- control.php
+++ control.php
@@ -4,13 +4,24 @@
 multi line comment
 */
 
-$mainPage = '<div><form action="javascript:;" onsubmit="submitStudent()"><input type="text" id="stdInput" style="margin:auto"></div>';
+$mainPage = '<div><form action="javascript:;" onsubmit="submitStudent()"><input type="text" id="stdInput" style="margin:auto"></form></div>';
 
 // inital load and anytime i would like to kick back inital page quickly
 if($action == 'load'){
+	unset($_SESSION['std']);
 	// return basic page and kick back
 	echo $mainPage;
 	exit();
+}
+
+if($action == 'assignParent'){
+	if(!isset($par)){throw new Exception('Required variables error.');}
+	if(isset($new)){
+		$theDBA->addParent($new);
+	}
+	$theDBA->assignParent($_SESSION['std'], $par);
+	echo 'ID added to account ' . $par . '<br>';
+	$action == 'submitStudent';
 }
 
 if($action == 'submitStudent'){
@@ -19,27 +30,32 @@ if($action == 'submitStudent'){
 		exit();
 	}
 	$_SESSION['std'] = $ID;
-	$par = retrieveParent($std);
+	$par = $theDBA->retrieveParent($std);
 
-	if($par == ''){
-		$allP = parList();
-		$options = '<>';
+	if($par == 'No parents found for this student.'){
+		$allP = $theDBA->parentsList();
+		$options = '<form action="javascript:;" onsubmit="assignStudent()"><label for="parents">Assign student to a Parent:</label><select name="parents", id="parents">';
 		foreach($allP as $p){
 			$options = $options . '<>' . $p . '</>';
 		}
-		$options = $options . '</>';
-		echo '' . $options . '';
+		$options = $options . '<option value="newParent">New Parent</>';
+		echo '' . $options . '<br><br><label for="newPar">Assign to new parent:</label><input type="text" id="newPar"><br><br><input type="submit" value="Submit"></form>';
 		exit();
 	}else{
-	$bal = retrieveBal($par);
-	echo '<h3>Snack Bar Purchase</h3><div><p>Account Info:</p> <p>Name: ' . $par . '</p> <p>Balance: $' . $bal . '</p> </div><form onsubmit="javascript" ="purchase()"><imput type="text"><br><p>Hot Lunch Purchase</p><imput type="text" id=""><input type="submit" value="Purchase"></form>';
+	$bal = $theDBA->retrieveBal($par);
+	echo '<h3>Snack Bar Purchase</h3><div><p>Account Info:</p> <p>Name: ' . $par . '</p> <p>Balance: $' . $bal . '</p> </div><form onsubmit="javascript" ="purchase()"><input type="text"><br><p>Hot Lunch Purchase</p><input type="text" id=""><input type="submit" value="Purchase"></form>';
 	exit();
 	}
 }
 
 if($action == 'pay'){
 	if(isset($snack) && isset($hotlunch) && isset($_SESSION['std'])){
-		$par = retrieveParent($_SESSION['std']);
+		$par = $theDBA->retrieveParent($_SESSION['std']);
+		
+		// convert to cents for database use
+		$snack = intval($snack*100);
+		$hotlunch = intval($hotlunch*100);
+		
 		if($par == 'No parents found for this student.'){
 			throw new Exception('Invalid student error.');
 		}elseif($snack < 0 || $hotlunch < 0){
@@ -48,15 +64,23 @@ if($action == 'pay'){
 			throw new Exception('Isufficient funds error.');
 		}
 
-		purchase($_SESSION['std'], $snack, $hotlunch);
+		$theDBA->purchase($_SESSION['std'], $snack, $hotlunch);
 		$o = '';
-		if($snack > 0){$o = $o . '<p>Purchased snacks worth $' . $snack . ' from the snack bar.</p>';}
-		if($hotlunch > 0){$o = $o . '<p>Purchased hot lunch(es) worth $' . $hotlunch . ' from the snack bar.</p>';}
+		if($snack > 0){$o = $o . '<p>Purchased snacks worth $' . $snack/100 . ' from the snack bar.</p>';}
+		if($hotlunch > 0){$o = $o . '<p>Purchased hot lunch(es) worth $' . $hotlunch/100 . ' from the snack bar.</p>';}
 		echo $o . $mainPage;
+		unset($_SESSION['std']);
 		exit();
 	}else{
 		throw new Exception('Invalid access error.');
 	}
+}
+
+if($action == 'history'){
+	if(!isset($_SESSION['std'])){
+		throw new Exception('No student error.');
+	}
+	// DONOTDO. this is for returning the history of the purchases. low priority if i can get the rest working
 }
 
 /*
