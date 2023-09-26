366511c14794bd1de73bbd3aa1119a89e06f77d6
TealDolphin <44395321+TealDolphin@users.noreply.github.com>
2023-09-26 01:27:25 -0700

    Now able to clear an ID from association with a student.

--- resetName/index.php
+++ resetName/index.php
@@ -2,21 +2,17 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="template.css" />
-<title>Page Title</title>
+<link rel="stylesheet" type="text/css" href="../template.css" />
+<title>Unassign ID</title>
 </head>
+
 <body>
-
-
-<!-- 
-static $mainPage = '<div><form action="javascript:;" onsubmit="submitStudent()" ><input type="text" autocomplete="off" placeholder="ID number input" id="stdInput" class="stdInput" pattern="^[0-9]{10}$"></form></div>';
--->
 <?php
 session_start();
 $_SESSION['state'] = 'ready';
 ?>
 <div id='main'>
-<div><form action="javascript:;" onsubmit="request()" id="form1"><h2>Input Student ID to Unassigned:<\h2><input type="text" autocomplete="off" placeholder="ID number input" id="stdInput" class="stdInput" pattern="^[0-9]{10}$"></form></div>
+<div><form action="javascript:;" onsubmit="request()" id="form1" class="wipeForm"><h2 id="headline">Input Student ID to Unassign:</h2><input type="text" autocomplete="off" placeholder="ID number input" id="stdInput" class="wipeID" pattern="^[0-9]{10}$"></form></div>
 </div>
 <script type="text/javascript" href="template.js">
 
@@ -27,34 +23,49 @@ function request(){
     let ajax = new XMLHttpRequest();
     let id = document.getElementById('stdInput').value;
     let f = document.getElementById('form1');
-    
-    ajax.open("GET", "control.php?action=load");
+	let headline = document.getElementById('headline');
+	
+	// remove previous no student found message
+	let negative = document.getElementById('Nope');
+	if(negative){
+		negative.remove();
+	}
+	
+	
+	//alert(id);
+	
+    ajax.open("GET", "special.php?action=request&ID=" + id);
     ajax.send();
     
     ajax.onreadystatechange = function(){
         if(ajax.readyState == 4 && ajax.status == 200){
-            f.innerHTML = "value=\"" + ajax.responseText + "\">";
-            f.onsubmit = "wipe()";
+			//alert(ajax.responseText);
+			if(ajax.responseText === "None"){
+				page.innerHTML = "<h3 id=\"Nope\">No students found for this ID.</h3>" + page.innerHTML;
+			}else{
+				// clear the form and set it to yes/no for wiping or not.
+				f.innerHTML = "";
+				headline.innerHTML = "Clear ID from student \"" + ajax.responseText + "\"?";
+				f.appendChild(headline);
+				f.innerHTML = f.innerHTML + "<input type=\"hidden\" id=\"ID\" value=\"" + id + "\">";
+				f.innerHTML = f.innerHTML + "<input type=\"button\" id=\"Yes\" name=\"Yes\" value=\"Yes\" class=\"wipeButton\" onclick=\"wipe()\"><input type=\"button\" onclick=\"location=location\" id=\"No\" name=\"No\" value=\"No\" class=\"wipeButton\">";
+			}
         }
     };
 }
 
 function wipe(){
     let ajax = new XMLHttpRequest();
+    let id = document.getElementById('ID').value;
     
-    
-    ajax.open("GET", "control.php?action=load");
+    ajax.open("GET", "special.php?action=clear&ID=" + id);
     ajax.send();
     
     ajax.onreadystatechange = function(){
         if(ajax.readyState == 4 && ajax.status == 200){
-            page.innerHTML = ajax.responseText;
+            page.innerHTML = "<h2>" + ajax.responseText + "</h2>";
         }
     };
-}
-
-function leave(){
-    page.innerHTML = "";
 }
 </script>
 </body>
--- resetName/special.php
+++ resetName/special.php
@@ -1,11 +1,19 @@
 <?php
-require_once "model.php";
+require_once "../model.php";
 session_start();
 
+if(isset($_GET['action']) && $_GET['action'] == 'request' && isset($_GET['ID'])){
+	//echo '' . $_GET['ID'];
+	$par = $theDBA->retrieveParent($_GET['ID']);
+	if($par == 'No parents found for this student.'){
+		echo 'None';
+		exit();
+	}
+	echo $par;
+}
 
-
-if(isset($_SESSION['clear'])){
-	unAssignId($_SESSION['clear']);
+if(isset($_GET['action']) && $_GET['action'] == 'clear' && isset($_GET['ID'])){
+	$theDBA->unAssignId($_GET['ID']);
 	echo 'ID cleared from student.';
 }
 ?>
