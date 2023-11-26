c9dc0e16422eee3b601f99c30e726cc16fd9180b
TealDolphin <44395321+TealDolphin@users.noreply.github.com>
2023-11-26 10:58:40 -0700

    Update reformat.php

--- reformat.php
+++ reformat.php
@@ -8,9 +8,16 @@ $i = 0;
 $return = $pieces[$i++];
 
 while($i < length($pieces)){
-  $return = $return . "'" . $pieces[$i++] . "'" . $pieces[$i++];
+  $return = $return . "'" . titleCase($pieces[$i++]) . "'" . $pieces[$i++];
 }
 
 
 // write output to file
+
+function titleCase($name){
+  $n = explode($name, ' ');
+  $r = '';
+  $i = 0;
+  if($n[$i] != 
+}
 ?>
