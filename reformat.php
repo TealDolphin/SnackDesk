0d120c571e9cc70e12a1687ddcd4c9e3a53b2a56
TealDolphin <44395321+TealDolphin@users.noreply.github.com>
2023-11-26 18:48:29 -0700

    Update reformat.php

--- reformat.php
+++ reformat.php
@@ -2,22 +2,34 @@
 $input = "";
 $return = "";
 
-$pieces = explode($input, "'");
+$pieces = explode("'", $input);
 
 $i = 0;
 $return = $pieces[$i++];
+$a = sizeof($pieces);
 
-while($i < length($pieces)){
+while($i < $a){
   $return = $return . "'" . titleCase($pieces[$i++]) . "'" . $pieces[$i++];
 }
-
+$myfile = fopen("clean.txt", "w") or die("AAAAAAAAAAAA");
+fwrite($myfile, $return);
+fclose($myfile);
 
 // write output to file
 
 function titleCase($name){
-  $n = explode($name, ' ');
-  $r = '';
+  $n = explode(' ', trim($name));
   $i = 0;
-  if($n[$i] != 
+  $r = ucfirst(strtolower(trim($n[$i++])));
+  $a = sizeof($n);
+  while($i < $a){
+	  $m = ucfirst(strtolower(trim($n[$i++])));
+	  if($m != ''){
+		  $r = $r . ' ' . $m;
+	  }
+  }
+  return $r;
 }
+
+//ucfirst(strtolower($m))
 ?>
